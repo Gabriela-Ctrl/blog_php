d20e3675ef1577db478ba176ab05ff55c4b01ba8
Gabriela-Ctrl <65233431+Gabriela-Ctrl@users.noreply.github.com>
2023-10-19 12:16:48 -0300

    ytres

--- data/posts.php
+++ data/posts.php
@@ -30,3 +30,52 @@
       'img' => 'prog-4.jpg'
     ],
   ];
+  ?>
+  <!-- <?php
+    include_once("templates/header.php");
+    
+    if(isset($_GET['id'])) {
+      
+      $postId = $_GET['id'];
+      $currentPost;
+
+      foreach($posts as $post) {
+        
+        if($post['id'] == $postId) {
+          $currentPost = $post;
+        }
+      }
+    }
+    
+  ?>
+
+  <main id="post-container">
+    <div class="content-container">
+      <h1 id="main-title"><?= $currentPost['title'] ?></h1>
+      <p id="post-description"><?+ $currentPost['description'] ?></p>
+      <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost ['title'] ?>">
+    
+    <p class="post-content"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum minima a dolores, deleniti alias provident esse, commodi enim doloribus ea in consequuntur consequatur molestiae fugit, quos atque reiciendis repellat ex! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque natus itaque velit voluptatibus nulla, culpa officia qui molestiae praesentium unde similique, fugit nemo enim aliquam omnis et, doloribus eaque sapiente! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis necessitatibus dolores dolore quaerat, vel earum nostrum, dicta magnam repudiandae enim quam sed. Excepturi sequi beatae possimus nobis! Totam, animi repudiandae!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum minima a dolores, deleniti alias provident esse, commodi enim doloribus ea in consequuntur consequatur molestiae fugit, quos atque reiciendis repellat ex! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque natus itaque velit voluptatibus nulla, culpa officia qui molestiae praesentium unde similique, fugit nemo enim aliquam omnis et, doloribus eaque sapiente! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis necessitatibus dolores dolore quaerat, vel earum nostrum, dicta magnam repudiandae enim quam sed. Excepturi sequi beatae possimus nobis! Totam, animi repudiandae!</p>
+    <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum minima a dolores, deleniti alias provident esse, commodi enim doloribus ea in consequuntur consequatur molestiae fugit, quos atque reiciendis repellat ex! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque natus itaque velit voluptatibus nulla, culpa officia qui molestiae praesentium unde similique, fugit nemo enim aliquam omnis et, doloribus eaque sapiente! Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+    <div class="nav-container">
+      <h3 id="tags-title">Tags</h3>
+      <ul id="tag-list">
+         <?php foreach($currentPost['tags'] as $tag): ?>
+         <li><a href="#"><?= $tag ?></a></li>
+         <?php endforeach; ?>
+      </ul>
+    
+    <h3 id="categories-title">Categorias</h3>
+    <ul id="categories-list">
+      <?php foreach($categories as $category): ?>
+        <li><a href="#"><?= $categories ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+    </div>
+  </main>
+ 
+  <?php
+  include_once("templates/footer.php");
+  ?> -->
--- index.php
+++ index.php
@@ -1,32 +1,58 @@
 <?php
+
     include_once("templates/header.php");
+
 ?>
 
-<!-- <h1>Olá Mundo! ! Bem-vindo ao PHP! !</h1> -->
+<!-- <h1>Olá Mundo!! Bem-vindo ao PHP</h1>-->
+
 <?php
-// var_dump($posts);
-// print_r($posts);
-// echo"<br><br>";
+
+    // var_dump($posts);
+    // print_r($posts);
+    // echo "<br><br>";
+
 ?>
 
 <main>
     <div id="title-container">
+
         <h1>Code Blog</h1>
-        <p>O seu blog de programação</p>
+        <p>O seu Blog de Programação</p>
+
     </div>
-    <div id="posts-container">
-    <?php foreach($posts as $post); ?>
-        <div class="post-box">
-        
-       
-        <img src="<?BASE_URL?>/img/<?=$post['img']?>"
-        alt="<?$post['title']?>">
-        <h2 class-"post-title"><a href="|"<?=BASE_URL?>">post.php? id=<?=$post[id]?>"></a></h2>
-        </div>
+    <div id="post-container">
+
+        <?php foreach($posts as $post): ?>
+
+            <div class="post-box">
+
+                <img src="<?=$BASE_URL ?>/img/<?=$post['img']?>" alt="<?=$post['title'] ?>">
+
+                <h2 class="post-title"><a href="<?=$BASE_URL?>post.php?id=<?=$post['id'] ?>"></a></h2>
+
+                <p class="post-description"><?= $post['description']?></p>
+                <div class="tags-container">
+                    <?php
+                     foreach ($post['tags'] as $tag):
+                    ?>
+
+                    <a href="#"><?=$tag?></a>
+
+                    <?php 
+                        endforeach;
+                    ?>
+                </div>  
+            
+            </div>
+        <?php 
+            endforeach;
+        ?>
     </div>
-</main>
+<main>
 
 <?php
-    include_once("templates/footer.php");
-?>
 
+    include_once("templates/footer.php");
+
+?>
